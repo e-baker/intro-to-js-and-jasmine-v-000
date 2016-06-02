b4688c7bc4069d7e21db38118cc3bc95f43c8231
e-baker <eric@trafficlight.me>
2016-06-01 20:38:04 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,13 @@
 'use strict';
+function favIceCream(flavor) {
+  return "I love " + flavor;
+}
+function shouting(message) {
+  return message.toUpperCase();
+}
+function roundDown(number) {
+  return Math.floor(number);
+}
+function theTruth() {
+  return true;
+}
